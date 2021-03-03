b44e88d59888982bdb01306b5af986453d6fc08a
xavier414 <xavierbryant@shaw.ca>
2021-03-03 15:09:47 +0100

    add data

--- app.R
+++ app.R
@@ -1,11 +1,19 @@
 library(shiny)
+library(gapminder)
+
+dataPanel <- tabPanel("Data",
+                      tableOutput("data")
+)
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(title="Shiny App",
-                h1("Hello"))
-
+ui <- navbarPage("shiny App",
+                 dataPanel
+)
 # Define server logic required to draw a histogram
-server <- function(input, output) { }
+server <- function(input, output) { 
+  output$data <- renderTable(gapminder)
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
