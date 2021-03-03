a13f0b528ec9b3de0d55583660b0a9a274b62b8a
xavier414 <xavierbryant@shaw.ca>
2021-03-03 15:22:09 +0100

    added histogram

--- app.R
+++ app.R
@@ -13,14 +13,24 @@ dataPanel <- tabPanel("Data",
                       tableOutput("data")
 )
 
+plotPanel <- tabPanel("Plot",
+                      plotOutput("plot")
+)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("shiny App",
-                 dataPanel
+                 dataPanel,
+                 plotPanel
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) { 
-  output$data <- renderTable(gapminder %>% filter(year %in% input$selYear))
+  output$data <- renderTable(gapminder %>% filter(year %in% input$selYear));
+  output$plot <- renderPlot(
+    barplot(head(gapminder %>% filter(year %in% input$selYear) %>% pull(pop)),
+            main=paste("Population in",input$selYear), horiz=FALSE,
+            names.arg= head(gapminder %>% filter(year %in% input$selYear) %>% pull(country))
+    )
+  )
 }
 
 # Run the application 
